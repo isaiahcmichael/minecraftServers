864764b34c0f527471fd37fb43c356a459ae39b1
Isaiah Michael <isaiahcmichael@proton.me>
2025-01-06 18:24:04 -0500

    added /servers/, and also merged Repo URL with title

--- servers/cc-survivalbedrock/script.js
+++ servers/cc-survivalbedrock/script.js
@@ -1,8 +1,5 @@
 const servers = [
-    { name: "Survival Server (Java)", url: "SoleStorm567603.aternos.me:25565", description: "The Survival Server for Java Edition! (Uses default ports for Minecraft Java)", infourl: "survivaljava" },
-    { name: "Survival Server (Bedrock)", url: "SoleStorm567603.aternos.me:50031", description: "The Survival Server for Java Edition, but translated using GeyserMC! (Make sure that the port is 50031)", infourl: "survivalbedrock" },
-    { name: "CamoCruise's Survival Server (Java)", url: "CamoCruise.aternos.me:25565", description: "CamoCruise's Survival Server! (Uses default port for Minecraft Java)", infourl: "cc-survivaljava" },
-    { name: "CamoCruise's Server (Bedrock)", url: "CamoCruise.aternos.me:57270", description: "CamoCruise's Survival Server! Translated to Bedrock using GeyserMC! (Make sure that the port is 57270)", infourl: "cc-survivalbedrock" },
+    { name: "CamoCruise's Server (Bedrock)", url: "CamoCruise.aternos.me:57270", description: "CamoCruise's Survival Server! Translated to Bedrock using GeyserMC! (Make sure that the port is 57270)", infourl: "cc-survivalbedrock", owner: "CamoCruise" },
 ];
 
 function loadServers() {
@@ -13,8 +10,9 @@ function loadServers() {
 
         serverCard.innerHTML = `
         <h2>${server.name}</h2>
-        <p><strong>URL:</strong> <a href="servers/${server.infourl}/index.html" target="_blank">${server.url}</a></p>
+        <p><strong>URL:</strong> <a href="index.html" target="_blank">${server.url}</a></p>
         <p>${server.description}</p>
+        <p>Owned By: ${server.owner}</p>
         `;
 
         serverList.appendChild(serverCard);
